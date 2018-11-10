3236a37238b285511a00845d8a046c43a4372970
Alexander Dießl <42088160+adiessl@users.noreply.github.com>
2018-11-10 15:19:02 +0100

    Refactor function "getSong"

--- ts/src/get-song.ts
+++ ts/src/get-song.ts
@@ -1,32 +1,11 @@
 import { getVerse } from './get-verse';
 
-// TODO: refactor
 export function getSong(): string {
-  let song = 'The Twelve Days of Christmas';
-  song += '\n\n';
-  song += getVerse(1);
-  song += '\n\n';
-  song += getVerse(2);
-  song += '\n\n';
-  song += getVerse(3);
-  song += '\n\n';
-  song += getVerse(4);
-  song += '\n\n';
-  song += getVerse(5);
-  song += '\n\n';
-  song += getVerse(6);
-  song += '\n\n';
-  song += getVerse(7);
-  song += '\n\n';
-  song += getVerse(8);
-  song += '\n\n';
-  song += getVerse(9);
-  song += '\n\n';
-  song += getVerse(10);
-  song += '\n\n';
-  song += getVerse(11);
-  song += '\n\n';
-  song += getVerse(12);
+  const verses = new Array(12)
+    .fill(0)
+    .map((_, index) => index + 1)
+    .map((day) => getVerse(day))
+    .join('\n\n');
 
-  return song;
+  return `The Twelve Days of Christmas\n\n${verses}`;
 }
